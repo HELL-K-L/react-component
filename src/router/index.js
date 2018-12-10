999628728c413402e8b9e125270b077f1ba665d8
kylin <huangql@shishike.com>
2018-12-10 11:07:48 +0800

    按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,23 @@
 import React from "react";
+
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Home from "pages/Hello";
-import Page from "pages/Page";
-import Counter from "pages/Counter";
-import UserInfo from "pages/UserInfo";
+
+import Bundle from "./Bundle";
+
+import Hello from "bundle-loader?lazy&name=home!pages/Hello";
+import Page from "bundle-loader?lazy&name=page1!pages/Page";
+import Counter from "bundle-loader?lazy&name=counter!pages/Counter";
+import UserInfo from "bundle-loader?lazy&name=userInfo!pages/UserInfo";
+
+const Loading = function() {
+  return <div>Loading...</div>;
+};
+
+const createComponent = component => props => (
+  <Bundle load={component}>
+    {Component => (Component ? <Component {...props} /> : <Loading />)}
+  </Bundle>
+);
 
 const getRouter = () => (
   <Router>
@@ -16,17 +30,17 @@ const getRouter = () => (
           <Link to="/page">Page</Link>
         </li>
         <li>
-          <Link to="/counter">counter</Link>
+          <Link to="/counter">Counter</Link>
         </li>
         <li>
           <Link to="/userinfo">UserInfo</Link>
         </li>
       </ul>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/page" component={Page} />
-        <Route path="/counter" component={Counter} />
-        <Route path="/userinfo" component={UserInfo} />
+        <Route exact path="/" component={createComponent(Hello)} />
+        <Route path="/page" component={createComponent(Page)} />
+        <Route path="/counter" component={createComponent(Counter)} />
+        <Route path="/userinfo" component={createComponent(UserInfo)} />
       </Switch>
     </div>
   </Router>
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -9,7 +9,8 @@ module.exports = {
   /*输出到dist文件夹，输出文件名字为bundle.js*/
   output: {
     path: path.join(__dirname, "./dist"),
-    filename: "[name].js"
+    filename: "[name].[hash].js",
+    chunkFilename: "[name].[chunkhash].js"
   },
   module: {
     rules: [
